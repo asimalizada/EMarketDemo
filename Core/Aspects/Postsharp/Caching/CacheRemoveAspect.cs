3882fb49cfb7ed6192122ad3c62a720b436e4af2
AsimAlizade <asimelizade01@gmail.com>
2021-06-01 12:30:39 +0400

    Caching added

---
+++ Core/Aspects/Postsharp/Caching/CacheRemoveAspect.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reflection;
+using Core.CrossCuttingConcerns.Caching;
+using PostSharp.Aspects;
+
+namespace Core.Aspects.Postsharp.Caching
+{
+    [Serializable]
+    public class CacheRemoveAspect : OnMethodBoundaryAspect
+    {
+        private string _pattern;
+        private Type _cacheType;
+        private ICacheManager _cacheManager;
+
+        public CacheRemoveAspect(Type cacheType, string pattern)
+        {
+            _cacheType = cacheType;
+            _pattern = pattern;
+        }
+
+        public CacheRemoveAspect(Type cacheType)
+        {
+            _cacheType = cacheType;
+        }
+
+        public override void RuntimeInitialize(MethodBase method)
+        {
+            if (!typeof(ICacheManager).IsAssignableFrom(_cacheType))
+            {
+                throw new Exception("Wrong cache type");
+            }
+
+            _cacheManager = (ICacheManager)Activator.CreateInstance(_cacheType);
+
+            base.RuntimeInitialize(method);
+        }
+
+        public override void OnSuccess(MethodExecutionArgs args)
+        {
+            _cacheManager.RemoveByPattern(
+                string.IsNullOrEmpty(_pattern) 
+                    ? string.Format("{0}.{1}.*",
+                    args.Method.ReflectedType.Namespace,
+                    args.Method.ReflectedType.Name) : _pattern);
+        }
+    }
+}
--- EMarketDemo.Business/Concrete/ProductManager.cs
+++ EMarketDemo.Business/Concrete/ProductManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Core.Aspects.Postsharp.Caching;
 using Core.CrossCuttingConcerns.Validation.FluentValidation;
 using EMarketDemo.Business.Abstract;
 using EMarketDemo.Business.CrossCuttingConcerns.Validation.FluentValidation;
@@ -10,6 +11,7 @@ using EMarketDemo.Entities.Dtos;
 using FluentValidation;
 using Core.Aspects.Postsharp.Transaction;
 using Core.Aspects.Postsharp.Validation;
+using Core.CrossCuttingConcerns.Caching.Microsoft;
 
 namespace EMarketDemo.Business.Concrete
 {
@@ -23,60 +25,72 @@ namespace EMarketDemo.Business.Concrete
         }
 
         [ValidationAspect(typeof(ProductValidator))]
+        [CacheRemoveAspect(typeof(MemoryCacheManager))]
         public void Add(Product product)
         {
+            // ...
             _productDal.Add(product);
         }
-
+        
+        [CacheRemoveAspect(typeof(MemoryCacheManager))]
         [ValidationAspect(typeof(ProductValidator))]
         public void Update(Product product)
         {
             _productDal.Update(product);
         }
 
+        [CacheRemoveAspect(typeof(MemoryCacheManager), "Get")]
         public void Delete(Product product)
         {
             _productDal.Delete(product);
         }
 
+        [CacheAspect(typeof(MemoryCacheManager))]
         public List<Product> GetAll()
         {
             return _productDal.GetAll();
         }
 
+        [CacheAspect(typeof(MemoryCacheManager))]
         public Product GetById(int id)
         {
             return _productDal.Get(p => p.Id == id);
         }
 
+        [CacheAspect(typeof(MemoryCacheManager))]
         public List<Product> GetByProductName(string productName)
         {
             return _productDal.GetAll(p => p.ProductName
                 .ToLower().Contains(productName.ToLower()));
         }
 
+        [CacheAspect(typeof(MemoryCacheManager))]
         public List<Product> GetByCategory(int categoryId)
         {
             return _productDal.GetAll
                 (p => p.CategoryId == categoryId);
         }
 
+        [CacheAspect(typeof(MemoryCacheManager))]
         public List<Product> GetByPrice(decimal min, decimal max)
         {
             return _productDal.GetAll
                 (p => p.Price >= min & p.Price <= max);
         }
 
+        [CacheAspect(typeof(MemoryCacheManager))]
         public decimal GetMinPrice()
         {
             return _productDal.GetAll().Select(p => p.Price).Min();
         }
 
+        [CacheAspect(typeof(MemoryCacheManager))]
         public decimal GetMaxPrice()
         {
             return _productDal.GetAll().Select(p => p.Price).Max();
         }
 
+        [CacheAspect(typeof(MemoryCacheManager))]
         public List<ProductDetailsDto> GetProductDetails()
         {
             return _productDal.GetProductDetails();
